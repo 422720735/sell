baf2414dac195d7554e867a25709d32ffa7ca2b2
刘体勇 <liutiyong@liutiyongdeMacBook-Pro.local>
2019-02-14 19:24:28 +0800

    完成购物车等交互功能

--- __tests__/App.js
+++ __tests__/App.js
@@ -10,6 +10,7 @@ import App from '../src/App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
