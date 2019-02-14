45e351e8e6328f8cda81906524cd669edbce919e
刘体勇 <liutiyong@liutiyongdeMacBook-Pro.local>
2019-02-14 19:16:41 +0800

    完成购物车等交互功能

--- index.js
+++ index.js
@@ -6,7 +6,4 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-
-// 导航器配置文件
-import AppNavigator from './src/js/navigator/AppNavigator'
 AppRegistry.registerComponent(appName, () => App);
